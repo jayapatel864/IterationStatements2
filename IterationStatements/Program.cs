bcc34bdacc0b2bf0e93152dead585bd9f4c5bc64
unknown <jayapatel864@gmail.com>
2020-06-24 13:44:47 -0500

    6/24 completed

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace IterationStatements
 {
@@ -7,45 +8,80 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-            //TODO
 
-            //Create a List called "numbers"
-            //Create a variable of type int with an initializer of 0
-
-
+            var numbers = new List<int>();
 
             // Create a do-while loop
+            int init = 0;
+            do
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+
+                init++;
+                numbers.Add(init);
+
+            } while (init < 100);
+            // Increment your variable by 1
+            // Then add your variable to "numbers"
+            
+            while(init < 200)
+            {
+                init++;
+                numbers.Add(init);
+
+
+
             }
+
             // While your variable is less than 100
+            Console.WriteLine("Increase");
+            foreach (var num in numbers)
+            {
+                Console.WriteLine($"{num}");
+                
+
+            }
+
+            Console.WriteLine("Decrease");
+
+            // Create a for loop
+            // DONE in your initializer set the value of i to 199
+            // in your conditional, as long as i is less than or equal to the length of "numbers"
+            // AND as long as i is greater than or equal to 0
+            // Decrement i by 1
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
+            {
+
+                Console.WriteLine($"{numbers[i]}");
+
+            }
+        }
+    }
+}
+
+
+
+
 
 
 
             // Create a while loop
             // While your variable is less than 200
-            {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
-            }
 
-            Console.WriteLine("Increase:");
+//Console.WriteLine("Increase:"); Increment your variable by 1
+// Then add your variable to "numbers"
 
-            // Create a foreach loop
-            // Write your variable to the console
 
-            Console.WriteLine("");
-            Console.WriteLine("Decrease:");
 
-            // Create a for loop
-            // in your initializer set the value of i to 199
-            // in your conditional, as long as i is less than or equal to the length of "numbers"
-            // and as long as i is greater than or equal to 0
-            // Decrement i by 1
-            {
-                // Write to the console "numbers" at index i
-            }
-        }
-    }
-}
+
+// Create a foreach loop
+// Write your variable to the console
+
+
+
+// Create a for loop
+// in your initializer set the value of i to 199
+// in your conditional, as long as i is less than or equal to the length of "numbers"
+// and as long as i is greater than or equal to 0
+// Decrement i by 1
+
+// Write to the console "numbers" at index i
